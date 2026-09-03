344a7564a3b849b3d17712f61e3447c53da5ca57
cloudjumper <162805460+xaviersupreme@users.noreply.github.com>
2026-09-02 19:29:45 -0700

    actually the final acb update

--- src/acb.lua
+++ src/acb.lua
@@ -1,7 +1,5 @@
 --- shit probably doesnt even work but ion care
 
-setthreadidentity(3)
-
 getgenv().adonisBypass = false
 
 const function notify(text, duration)
@@ -21,37 +19,36 @@ local oldDestroy; oldDestroy = hookmetamethod(game, "__index", newcclosure(funct
 end))
 
 const yieldFunction = (function(...) while true do wait(0xFF) end end)
+const newDetectionProxyFunction = (function() while true do wait(0xFF) end return "" end)
 
 local checkStackFunction = filtergc("function", { Hash = "801b76891f73d63ec073100922d1d5bde4af00e0ad4d891f096224f94370c40a5bc12f70eacf69530f199d4f000da8d0", IgnoreExecutor = true }, true)
-hookfunction(checkStackFunction, yieldFunction)
-
 local detectionProxyFunction = filtergc("function", { Hash = "d8b9e99aeebbc83f5937f0d226b1a14de0d0aaeb516e8b7335d544ab06443178b5c20e360c3e42b9fd21e33bedcff11e", IgnoreExecutor = true }, true)
-local newDetectionProxyFunction = function() while true do wait(0xFF) end return "" end
-local bindableEvent = debug.getupvalue(detectionProxyFunction, 1) -- ReplicatedStorage._sigma :3 (probably)
-hookfunction(detectionProxyFunction, newDetectionProxyFunction)
-
 local memoryFloodFunction = filtergc("function", { Name = "FLOOD_MEMORY", IgnoreExecutor = true }, true)
+local metamethodChecksFunction = filtergc("function", { Hash = "98ead62c64aabce7b4d7780be90367a521c3863717110f4251875fe873804bd6e294990723a8be90c4386d2cb33cd855", IgnoreExecutor = true }, true)
+
+hookfunction(checkStackFunction, yieldFunction)
+hookfunction(detectionProxyFunction, newDetectionProxyFunction)
+hookfunction(metamethodChecksFunction, yieldFunction)
+
+local bindableEvent = debug.getupvalue(detectionProxyFunction, 1) -- ReplicatedStorage._sigma :3 (probably)
+
 local oldTaskSpawn; oldTaskSpawn = hookfunction(task.spawn, function(...)  
     if checkcaller() then return oldTaskSpawn(...) end
-
-    local args = {...}
-
-    if typeof(args[1]) == "function" and args[1] == memoryFloodFunction then
+    
+    local arg1 = select(1, ...)
+    if rawequal(typeof(arg1), "function") and rawequal(arg1, memoryFloodFunction) then
         return
     end
 
     return oldTaskSpawn(...)
 end)
 
-local metamethodChecksFunction = filtergc("function", { Hash = "98ead62c64aabce7b4d7780be90367a521c3863717110f4251875fe873804bd6e294990723a8be90c4386d2cb33cd855", IgnoreExecutor = true }, true)
-hookfunction(metamethodChecksFunction, yieldFunction)
-
 local infoFunction = debug.info
 local oldInfo; oldInfo = hookfunction(infoFunction, newcclosure(function(level, options)
     if checkcaller() then return oldInfo(level, options) end
     if typeof(level) ~= "number" then return oldInfo(level, options) end
 
-    if typeof(level) == "number" and number >= 0 and number <= 20 then
+    if typeof(level) == "number" and level >= 0 and level <= 20 then
         if options == "l" then
             return nil
         end
@@ -60,67 +57,59 @@ local oldInfo; oldInfo = hookfunction(infoFunction, newcclosure(function(level, 
     return oldInfo(level, options)
 end))
 
-local oldBindable; oldBindable = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
+local oldNamecall; oldNamecall = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
     local method = getnamecallmethod()
-    if self == detectionProxyBindableEvent and (method == "Fire" or method == "fire") then
-        return
-    end
-
-    return oldBindable(self, ...)
-end))
-
-local oldProxyCaller; oldProxyCaller = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
-    local method = getnamecallmethod()
-    local args = {...}
-
-    if (method == "Fire" or method == "fire") and typeof(args[1]) == "table" then
-        for key, _ in args[1] do
-            if typeof(key) == "userdata" then
-                return
+    
+    if method == "Fire" or method == "fire" then
+        if self == detectionProxyBindableEvent then
+            return
+        end
+        
+        local arg1 = select(1, ...)
+        if typeof(arg1) == "table" then
+            for key, _ in arg1 do
+                if typeof(key) == "userdata" then
+                    return
+                end
             end
         end
     end
 
-    return oldProxyCaller(self, ...)
+    return oldNamecall(self, ...)
 end))
-
-setthreadidentity(8)
 
 local bypassSuccess = false
 task.spawn(function()
+    local thread2
     local thread = task.spawn(function() 
         game:GetService("Players").LocalPlayer.PlayerGui.ChildAdded:Connect(function(inst) 
             local name = tostring(inst.Name)
-
             if name:lower():find("terminal") then
                 repeat task.wait() inst:Destroy() until not inst
                 return
             end
         end)
-        
-        task.spawn(function() 
+        thread2 = task.spawn(function()
             while task.wait(0.5) do -- unoptimized but fuck you
                 for _, func in filtergc("function", {}) do
                     local s = debug.info(func, "s")
                     if not tostring(s):lower():find("crash") then continue end
-                    local o; o = hookfunction(func, function(...) 
+                    local o; o = hookfunction(func, function(...)
                         error()
                     end)
                 end
             end
         end)
-        
-        if getgenv().adonisBypass then
+        if adonisBypass then
             loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaomao8090/Adonis-Bypass-Framework/master/AdonisBypass.lua"), "adonis bypass")()
         end
-
         bypassSuccess = true
-    end); task.wait(30); task.cancel(thread)
+    end); task.wait(10); task.cancel(thread); task.cancel(thread2);
 end)
 
-notify("pwning anticheat hold up", 10)
+notify("pwning anticheat hold up", 5)
 repeat wait() until bypassSuccess
 notify("ok done have fun", 5)
 notify("also u might lag in a sec jus wait", 5)
 
-return { bypassSuccess }
+return bypassSuccess
